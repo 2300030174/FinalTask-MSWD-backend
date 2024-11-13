975466dc8dae5947d345d8056a642fb9d58b232b
2300030174 <2300030174@kluniversity.in>
2024-11-13 14:35:20 +0530

    first commit

---
+++ models/Faculty.js
@@ -0,0 +1,19 @@
+// backend/models/Faculty.js
+const mongoose = require('mongoose');
+
+const facultySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  department: {
+    type: String,
+    required: true,
+  },
+  designation: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Faculty', facultySchema);
